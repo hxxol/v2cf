8897925034453d930380f497c2585bd7966600d0
hxxol <x@oiago.com>
2025-11-07 21:22:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ sleep 2
 echo "[entrypoint] 启动 cloudflared 隧道，转发到 127.0.0.1:1988 ..."
 
 # 运行 cloudflared 并将流量转发到本地 1988 端口
-exec cloudflared tunnel --url http://127.0.0.1:1988 run --token eyJhIjoiOWU0Y2U4YTRhZWQzOTUxN2ZlNTlkNzJmZDI4MjY4OTgiLCJ0IjoiMDc5YzNkOTQtMzgzMi00ZDNhLWE4ZmYtNDEyNTc1OTNiOTc5IiwicyI6Ik5HVmxZemxsWXpJdFlXUXpOeTAwTnpSaUxXSTNNakl0TmpJeU1HSXpOVFU0T0dKbCJ9
+exec cloudflared tunnel --url http://127.0.0.1:1988 run --token eyJhIjoiOWU0Y2U4YTRhZWQzOTUxN2ZlNTlkNzJmZDI4MjY4OTgiLCJ0IjoiMDc5YzNkOTQtMzgzMi00ZDNhLWE4ZmYtNDEyNTc1OTNiOTc5IiwicyI6Ill6VmlNREpqTXprdFpUTTNOaTAwTURSaExUa3lOV0V0Wm1KaVlqQmhZakU0WWpSaiJ9
